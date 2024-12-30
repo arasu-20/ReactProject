0a9d9328f0a8325f900f60a6533e4b6247ecf042
arasu20 <imarasu20@gmail.com>
2024-12-30 19:01:16 +0530

    useReducer

--- Todo/src/components/useReducer/useReducer.jsx
+++ Todo/src/components/useReducer/useReducer.jsx
@@ -1,7 +1,42 @@
+import { useReducer } from "react";
+
+const initialState = {
+    showText : false,
+    changeStyle: false
+}
+const HIDE_TEXT = 'HIDE_TEXT';
+const SHOW_TEXT = 'SHOW_TEXT';
+const TOGGLE = 'TOGGLE';
+function reducer(state, action){
+    switch(action.type){
+        case SHOW_TEXT:
+            return {
+                ...state,
+                showText: true
+            }
+        case HIDE_TEXT:
+            return {
+                ...state,
+                showText: false
+            }
+        case TOGGLE:
+            return {
+                ...state,
+                changeStyle: !state.changeStyle
+                
+            }
+        default:
+            return state;
+    }
+}
 export default function UseReducerExample(){
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (<div>
-        <button>Hide Text</button>
-        <button>Show Text</button>
-        <button>Change Text</button>
+        {
+            state.showText ? <h3 style={{backgroundColor: state.changeStyle ? 'black' : 'blue', color: state.changeStyle ? 'yellow' : "black", fontSize: state.changeStyle ? '100px' : '50px'}}>Hello World!</h3> : null
+        }
+        <button onClick={()=>dispatch({type: HIDE_TEXT})}>Hide Text</button>
+        <button onClick={()=>dispatch({type: SHOW_TEXT})}>Show Text</button>
+        <button onClick={()=>dispatch({type: TOGGLE})}>Change Text</button>
     </div>);
 }
